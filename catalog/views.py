ad0d83e265a1c40071ed2acde0db7e3c0b81bd45
khaliliche <khalilichee@gmail.com>
2026-08-02 03:14:40 +0100

    test redeploy

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,21 @@
+from django.db.models import Case, When, Value, IntegerField
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
 def product_list(request, category_type):
     categories = Category.objects.filter(type=category_type)
-    products = Product.objects.filter(category__type=category_type, is_active=True)
+    products = (
+        Product.objects.filter(category__type=category_type, is_active=True)
+        .annotate(
+            priority=Case(
+                When(capacity_btu__in=[9000, 12000], then=Value(0)),
+                default=Value(1),
+                output_field=IntegerField(),
+            )
+        )
+        .order_by('priority', 'price')
+    )
 
     brand = request.GET.get('brand')
     if brand:
